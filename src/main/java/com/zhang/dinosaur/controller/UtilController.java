bc2b2aef2e77517252dcbcd8944a28327a2e5979
orange <982654045@qq.com>
2023-03-21 10:06:03 +0800

    Sets the bits from the specified {@code fromIndex} (inclusive) to the specified {@code toIndex} (exclusive) to the specified value and return long[]

---
+++ src/main/java/com/zhang/dinosaur/controller/UtilController.java
@@ -0,0 +1,34 @@
+package com.zhang.dinosaur.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.BitSet;
+
+@RestController
+@RequestMapping("util")
+@Slf4j
+public class UtilController {
+
+    /**
+     * Sets the bits from the specified {@code fromIndex} (inclusive) to the
+     * specified {@code toIndex} (exclusive) to the specified value
+     * @param bytes
+     * @param fromIndex
+     * @param toIndex
+     * @param value
+     * @return long[]
+     */
+    @PostMapping("bitSet")
+    public long[] bitSet(byte[] bytes,int fromIndex, int toIndex, boolean value){
+        BitSet bitSet = BitSet.valueOf(bytes);
+        bitSet.set(fromIndex,toIndex,value);
+        long[] array = bitSet.toLongArray();
+        String s = bitSet.toString();
+        log.info("return true bit indexs = {}",s);
+        return array;
+    }
+}
