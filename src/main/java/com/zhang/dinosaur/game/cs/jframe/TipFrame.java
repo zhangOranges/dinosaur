037180604c6cb04111d291d494755ee925b7ba0c
orange <982654045@qq.com>
2023-04-11 10:50:39 +0800

    add Constructor

--- src/main/java/com/zhang/dinosaur/game/cs/jframe/TipFrame.java
+++ src/main/java/com/zhang/dinosaur/game/cs/jframe/TipFrame.java
@@ -16,7 +16,18 @@ public class TipFrame extends JFrame {
         setSize(400, 200);
 
         JPanel jPanel = new JPanel(new MigLayout("align 50% 50%"));
-        jPanel.add(new JLabel("msg"));
+        jPanel.add(new JLabel(msg));
+        add(jPanel);
+        setLocationRelativeTo(null);
+        ThreadUtils.sleep(100);
+        setVisible(true);
+    }
+
+    public TipFrame(JPanel jPanel){
+        super("TipFrame view");
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setResizable(false);
+        setSize(400, 200);
         add(jPanel);
         setLocationRelativeTo(null);
         ThreadUtils.sleep(100);
