83e41e26a77802b32c97ef22fe4e68bbe7f169c3
orange <982654045@qq.com>
2023-04-11 10:34:23 +0800

    modify TipFrame

--- src/main/java/com/zhang/dinosaur/game/TipFrame.java
+++ src/main/java/com/zhang/dinosaur/game/TipFrame.java
@@ -1,20 +1,28 @@
 package com.zhang.dinosaur.game;
 
+import cn.hutool.core.util.StrUtil;
 import com.zhang.dinosaur.common.ThreadUtils;
+import net.miginfocom.swing.MigLayout;
+import org.apache.commons.codec.binary.StringUtils;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * 提示界面
  */
 public class TipFrame extends JFrame {
-    public TipFrame(){
+    public TipFrame(String msg){
         super("TipFrame view");
         setDefaultCloseOperation(HIDE_ON_CLOSE);
         setResizable(false);
         setSize(400, 200);
+
+        JPanel jPanel = new JPanel(new MigLayout("align 50% 50%"));
+        jPanel.add(new JLabel("msg"));
+        add(jPanel);
         setLocationRelativeTo(null);
-        ThreadUtils.sleep(2000);
+        ThreadUtils.sleep(100);
         setVisible(true);
     }
 }
