5d784f861b9b1827643441bd02a1c85bbcc5333e
orange <982654045@qq.com>
2023-04-13 15:24:38 +0800

    add bottom bar and fc button

--- src/main/java/com/zhang/dinosaur/game/cs/jpanel/MainToolBarPanel.java
+++ src/main/java/com/zhang/dinosaur/game/cs/jpanel/MainToolBarPanel.java
@@ -1,22 +1,44 @@
 package com.zhang.dinosaur.game.cs.jpanel;
 
 import net.miginfocom.swing.MigLayout;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 
 import javax.swing.*;
 import java.awt.*;
 
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER;
+
 public class MainToolBarPanel extends JPanel {
     public MainToolBarPanel() {
         //todo   add bar
-        super(new MigLayout("insets 5 5 5 5","[grow 40,fill][grow 60,fill]",""));
-        setBorder(BorderFactory.createLineBorder(Color.cyan));
+        super(new MigLayout("insets 10 10 -20 0","[grow 80,fill][grow 20,fill]",""));
+
+        setOpaque(false);
+
+
         JTextArea jTextArea = new JTextArea();
         jTextArea.setLineWrap(false);
         jTextArea.setWrapStyleWord(false);
-        jTextArea.setBackground(Color.MAGENTA);
-        add(jTextArea);
-        JLabel hello = new JLabel("hello");
-        hello.setBackground(Color.BLACK);
-        add(hello);
+        jTextArea.setOpaque(false);
+        jTextArea.setCaretColor(Color.green);
+        jTextArea.setForeground(Color.WHITE);
+        jTextArea.setFont(new Font("", Font.PLAIN, 20));
+
+
+        JScrollPane jScrollPane = new RoundedJScrollPane(jTextArea);
+        jScrollPane.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_NEVER);
+        jScrollPane.setBackground(new Color(0,0,0,150));
+
+        jScrollPane.getViewport().setOpaque(false);
+        jScrollPane.setOpaque(false);
+
+
+        add(jScrollPane);
+
+        //func button
+        Button button = new Button("b1");
+        add(button);
     }
 }
---
+++ src/main/java/com/zhang/dinosaur/game/cs/jpanel/RoundedJScrollPane.java
@@ -0,0 +1,22 @@
+package com.zhang.dinosaur.game.cs.jpanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedJScrollPane extends JScrollPane {
+    public RoundedJScrollPane(Component view) {
+        super(view);
+        setBorder(BorderFactory.createEmptyBorder());
+        setBackground(new Color(0,0,0,50));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 5, 5);
+        super.paintComponent(g2);
+        g2.dispose();
+    }
+}
