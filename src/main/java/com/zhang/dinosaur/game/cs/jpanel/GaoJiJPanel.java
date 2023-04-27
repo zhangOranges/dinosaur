90c97139b4fd270530ba6e407193ea54440e74e0
orange <982654045@qq.com>
2023-04-28 07:58:38 +0800

    ssh面板添加组件 完毕

--- src/main/java/com/zhang/dinosaur/game/cs/jpanel/GaoJiJPanel.java
+++ src/main/java/com/zhang/dinosaur/game/cs/jpanel/GaoJiJPanel.java
@@ -1,9 +1,16 @@
 package com.zhang.dinosaur.game.cs.jpanel;
 
+import net.miginfocom.swing.MigLayout;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class GaoJiJPanel extends JPanel {
     public GaoJiJPanel() {
+        super(new MigLayout("left,insets 20 30 0 0"));
         setBorder(BorderFactory.createTitledBorder("高级"));
+        JLabel nocode = new JLabel("待开发");
+        nocode.setForeground(Color.GRAY);
+        add(nocode);
     }
 }
--- src/main/java/com/zhang/dinosaur/game/cs/jpanel/RenZhengJPanel.java
+++ src/main/java/com/zhang/dinosaur/game/cs/jpanel/RenZhengJPanel.java
@@ -1,9 +1,36 @@
 package com.zhang.dinosaur.game.cs.jpanel;
 
+import net.miginfocom.swing.MigLayout;
+
 import javax.swing.*;
 
 public class RenZhengJPanel extends JPanel {
     public RenZhengJPanel() {
+
+        super(new MigLayout("left,insets 20 30 0 0"));
         setBorder(BorderFactory.createTitledBorder("认证"));
+        JLabel name = new JLabel("方法");
+        JTextField value = new JTextField();
+        add(name);
+        add(value,"wrap");
+
+        name = new JLabel("用户名");
+        value = new JTextField();
+        add(name);
+        add(value,"wrap");
+
+        name = new JLabel("密码");
+        value = new JPasswordField();
+        add(name);
+        add(value,"wrap");
+
+        name = new JLabel("私钥");
+        value = new JTextField();
+        add(name);
+        add(value);
+
+        JButton lookup = new JButton("浏览");
+        add(lookup);
+
     }
 }
